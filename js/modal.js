3b7faa9829a83adb4a55cc21a43809f2e3bc3d28
Yuliia Danylenko <yvdanilenko@gmail.com>
2021-07-10 20:20:06 +0300

    portfolio

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,13 @@
 ;(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  }
+  const menuBtnRef = document.querySelector('[data-menu-button]')
+  const mobileMenuRef = document.querySelector('[data-menu]')
+  const mobileBtnClose = document.querySelector('[data-menu-close]')
 
-  refs.openModalBtn.addEventListener('click', toggleModal)
-  refs.closeModalBtn.addEventListener('click', toggleModal)
+  menuBtnRef.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open')
+  })
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden')
-  }
+  mobileBtnClose.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open')
+  })
 })()
